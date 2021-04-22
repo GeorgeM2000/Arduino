955551d5a9d8fc4a758765ea49ae71395374dec0
George M <65071674+GeorgeM2000@users.noreply.github.com>
2021-04-22 22:07:18 +0300

    Update LiquidCrystal_Arduino.ino

--- LiquidCrystal_Arduino.ino
+++ LiquidCrystal_Arduino.ino
@@ -1,22 +1,24 @@
 #include <LiquidCrystal.h>
+
+// Create a LiquidCrystalObject
 LiquidCrystal lcd(11,12,4,5,6,7);
-String Msg = "This is my first LiquidCrystal Program.";
-unsigned int stringLength = Msg.length();
+
+String msg = "This is my first program using LiquidCrystal.";
+int msgLength = msg.length();
 
 void setup()
 {
-  lcd.begin(16,2);
-  lcd.setCursor(0,0);
-  lcd.print("Hello world!");
+  lcd.begin(16,2);                  // Specify how large the screen is
+  lcd.setCursor(0,0);              // Set the coordinates at the first column of the first row
+  lcd.print("Hello Everyone!");   // Write to the LCD screen
 }
 
 void loop()
 {
-  for( int i=0; i<stringLength ; i++ )
+  for(int i=0;i<msgLength;i++)
   {
-    lcd.setCursor(0,1);
-    lcd.print(Msg.substring(i));
-    lcd.print("                ");
-    delay(1000);
+    lcd.setCursor(0,1);                // Set the coordinates at the first column of the second row
+    lcd.print(msg.substring(i));      // Write to the LCD screen
+    delay(1000);                     // Wait 1 sec
   }
 }
