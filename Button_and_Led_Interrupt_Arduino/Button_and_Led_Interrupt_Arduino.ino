d2f0776c2eeadb8e2c4f23399dd7491206fc8c23
George M <giorgos_matlis@hotmail.com>
2021-04-29 22:03:06 +0300

    Update Button_and_Led_Interrupt_Arduino.ino

--- Button_and_Led_Interrupt_Arduino/Button_and_Led_Interrupt_Arduino.ino
+++ Button_and_Led_Interrupt_Arduino/Button_and_Led_Interrupt_Arduino.ino
@@ -1,5 +1,5 @@
 /*
-  In this circuit, i use a button, a led, a pull up resistor and i generate interrupts.
+  This script is used for a circuit in which i use a button, a led, a pull up resistor and i generate interrupts.
   When a transition occurs, that is, we transition from LOW voltage to HIGH and the other way around, the function ActivateLED is called.
   When this function is called the state variable changes, therefore the value of the led changes.
 */
