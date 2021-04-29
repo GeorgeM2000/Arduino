221e5938162e59045d38172f357fa7dffa9d724e
George M <giorgos_matlis@hotmail.com>
2021-04-29 14:42:36 +0300

    Update Ultrasonic_Sensor_With_Three_Leds_Arduino.ino

--- Ultrasonic_Sensor_With_Three_Leds_Arduino/Ultrasonic_Sensor_With_Three_Leds_Arduino.ino
+++ Ultrasonic_Sensor_With_Three_Leds_Arduino/Ultrasonic_Sensor_With_Three_Leds_Arduino.ino
@@ -1,25 +1,39 @@
-const int LEDpins[] = {10,11,12}; // Led pins 
+/* 
+  This script is used for a circuit in which i use an ultrasonic sensor to detect the distance from an obstacle.
+  Based on the distance of the obstacle, certain leds are activated.
+
+*/
 
 
+const int LEDpins[] = {10,11,12};                 // Digital pins for the Green, Yellow and Red LED 
 
 void setup(){
-  pinMode(2,OUTPUT);         // Digital pin 2 is connected to the trigger pin
-  pinMode(4,INPUT);         // Digital pin 4 is connected to the echo pin
+
+   // Digital pin 2 is connected to the trigger pin of the Ultrasonic sensor
+   // The trigger pin of the Ultrasonic sensor reads an activation signal transmited from the Arduino
+   // Arduino sends an activation signal to trigger pin so digital pin 2 is an output
+  pinMode(2,OUTPUT);  
+
+  // Digital pin 4 is connected to the echo pin 
+  // The Echo pin of the Ultrasonic sensor takes the information about the distacle of the obstacle                     
+  pinMode(4,INPUT);                         
   for(int i = 0; i < 3; i++) {
-    pinMode(LEDpins[i],OUTPUT);   // Set the led pins to OUTPUT
+    pinMode(LEDpins[i],OUTPUT);         // Set the led pins to OUTPUT
   }
 }
 
 void loop(){
   long Distance;
   
-  Distance = dis(2,4);        // Calculate the distance of the object
-  LED_display(Distance);      // Activate leds based on the distance of the object
+  Distance = dis(2,4);                    // Calculate the distance of the object
+  LED_display(Distance);                // Activate leds based on the distance of the object
   delay(500);
 }
 
 long dis(int TrigPin, int EchoPin){
   
+
+  // We use an activation signal to activate the sensor and transmit pulses
   // Activation signal -------------
   digitalWrite(TrigPin,LOW);
   delayMicroseconds(2);
@@ -29,18 +43,21 @@ long dis(int TrigPin, int EchoPin){
   delayMicroseconds(2);
   // -------------------------------
   
-  long Duration = pulseIn(EchoPin, HIGH);
-  return microsecondsToCentimeters(Duration);     //
+
+  // Arduino reads the Echo signal from the sensor through an input digital pin.
+  // The Echo signal is a pulse which indicates the time it took for the signal to leave and return from the obstacle
+  long Duration = pulseIn(EchoPin, HIGH);             
+  return microsecondsToCentimeters(Duration);         
 }
 
 
-// Function to determine what leds need to be activated
+// Function to determine how many leds need to be activated
 void LED_display(long distance){
-  if(distance >= 240){        // If the distance is greater than 240 cm, activate one led
+  if(distance >= 240){                        // If the distance is greater than 240 cm, activate one led(Green LED)
     LED_on(LOW,LOW,HIGH);}
-  else if(distance > 120){      // If the distance is greater than 120 cm, activate two leds
+  else if(distance > 120){                // If the distance is greater than 120 cm, activate two leds(Green and Yellow LED)
     LED_on(LOW,HIGH,HIGH);}
-  else if(distance < 90){     // If the distance is lower than 90 cm, activate all three leds
+  else if(distance < 90){             // If the distance is lower than 90 cm, activate all three leds(Green, Yellow and Red LED)
     LED_on(HIGH,HIGH,HIGH);}
 }
 
